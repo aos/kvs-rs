1a35618ee0ccae1e777ff693dbd4328d589e3351
Aos Dabbagh <aosdab@gmail.com>
2021-01-31 23:36:40 -0500

    finish first part

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,60 @@
+//! This library houses a key-value store
+
+#![deny(missing_docs)]
 use std::collections::HashMap;
+
+/// KvStore holds an in-memory HashMap of <String, String>
 pub struct KvStore {
     map: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// Returns a newly instantiated KvStore
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let k = KvStore::new();
+    /// ```
     pub fn new() -> Self {
         KvStore {
-            map: HashMap::new()
+            map: HashMap::new(),
         }
     }
 
+    /// Gets an item from the KvStore
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut k = KvStore::new();
+    ///
+    /// k.set("hi".to_owned(), "bye".to_owned());
+    /// assert_eq!(k.get("hi".to_owned()), Some("bye".to_owned()));
+    /// assert_eq!(k.get("no".to_owned()), None);
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
         match self.map.get(&key) {
             Some(v) => Some(v.clone()),
-            None => None
+            None => None,
         }
     }
 
+    /// Inserts an item or updates an existing item in the store
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut k = KvStore::new();
+    ///
+    /// k.set("hi".to_owned(), "bye".to_owned());
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.map.insert(key, value);
     }
 
+    /// Removes an item from the store
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut k = KvStore::new();
+    ///
+    /// k.set("hi".to_owned(), "bye".to_owned());
+    /// k.remove("hi".to_owned());
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.map.remove(&key);
     }
