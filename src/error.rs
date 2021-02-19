4bee430f5c862953ced3a51735dced40101c6322
Aos Dabbagh <aosdab@gmail.com>
2021-02-18 23:42:21 -0500

    add file handling for opening the kv store

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,13 @@
 use std::io;
+use std::time::SystemTimeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    Io(#[from] io::Error)
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    Time(#[from] SystemTimeError),
 }
 
 /// The Result type encapsulates standard result
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod command;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::fs::{self, File, OpenOptions};
-use std::time::{Instant, Duration};
+use std::time::{SystemTime, UNIX_EPOCH};
 pub use error::Result;
 use command::Command;
 
@@ -16,6 +16,7 @@ const EXT: &str = ".kvstore"; // {timestamp}.kvstore
 pub struct KvStore {
     map: HashMap<String, String>,
     dir: PathBuf,
+    active_file: File,
 }
 
 impl KvStore {
@@ -24,10 +25,11 @@ impl KvStore {
     /// use kvs::KvStore;
     /// let k = KvStore::new();
     /// ```
-    pub fn new(dir: PathBuf) -> Self {
+    pub fn new(dir: PathBuf, file: File) -> Self {
         KvStore {
             map: HashMap::new(),
             dir,
+            active_file: file,
         }
     }
 
@@ -77,8 +79,35 @@ impl KvStore {
         // 1. check to see if an existing file is available here
         // 2. if available, open it and slurp into memory (for now we will not buffer)
         //      - otherwise, create a new one
-        let current_dir = fs::read_dir(dir.into())?;
+        let current_dir: PathBuf = dir.into();
+        let maybe_latest = fs::read_dir(&current_dir)?
+            .filter_map(|e| e.ok())
+            .filter(|e| {
+                if let Ok(file_type) = e.file_type() {
+                    file_type.is_file() && e.path().extension().map_or(false, |e| e == "kvstore")
+                } else {
+                    false
+                }
+            })
+        .max_by_key(|e| {
+            e.file_name().to_str().map_or(0, |e| {
+                e.split(".").next().map_or(0, |v| v.parse::<usize>().unwrap_or(0))
+            })
+        });
 
-        Ok(())
+        let mut file = OpenOptions::new();
+        if let Some(found_latest) = maybe_latest {
+            let file: File = file.read(true).append(true).open(found_latest.path())?;
+            let mut kv = KvStore::new(current_dir, file);
+            let map = HashMap::new();
+            // Slurp the serialized data from file into hashmap
+
+            kv.map = map;
+            Ok(kv)
+        } else {
+            let ts = SystemTime::now().duration_since(UNIX_EPOCH)?;
+            let file = file.read(true).append(true).open(format!("{}.kvstore", ts.as_secs()))?;
+            Ok(KvStore::new(current_dir, file))
+        }
     }
 }
