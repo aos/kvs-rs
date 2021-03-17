b2fd4531cc8adc032a1ed69293f8c712298f940d
Aos Dabbagh <aosdab@gmail.com>
2021-03-17 10:34:17 -0400

    refactor server implementation

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,8 +1,10 @@
+use kvs::Result;
 use kvs::server::KvsServer;
-use slog::{o, Drain};
+use slog::{o, warn, Drain};
 use slog_async;
 use slog_term;
 use std::env;
+use std::fs;
 use std::net::SocketAddr;
 use structopt::StructOpt;
 
@@ -15,7 +17,7 @@ struct ServerOpts {
     engine: Option<String>,
 }
 
-fn main() -> kvs::Result<()> {
+fn main() -> Result<()> {
     let decorator = slog_term::PlainDecorator::new(std::io::stderr());
     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
@@ -24,9 +26,27 @@ fn main() -> kvs::Result<()> {
     let opts = ServerOpts::from_args();
 
     let logger = log.new(o!("addr" => opts.addr.to_string(), "engine" => opts.engine.to_owned()));
+
+    let curr_engine = current_engine()?.and_then(|e| {
+        if true {}
+    });
+
     let mut server = KvsServer::new(env::current_dir()?, opts.addr, opts.engine, &logger)?;
 
     server.start()?;
 
     Ok(())
+}
+
+fn current_engine() -> Result<Option<String>> {
+    let engine_file = env::current_dir()?.join("engine)");
+    if !engine_file.exists() {
+        return Ok(None)
+    }
+
+    match fs::read_to_string(engine_file)?.as_str() {
+        "Kvs" => Ok(Some("Kvs".to_owned())),
+        "Sled" => Ok(Some("Sled".to_owned())),
+        _ => Ok(None)
+    }
 }
